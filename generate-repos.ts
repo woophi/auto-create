9bb3bf966c238f4f5c85d8077aa55f14ed44a4bb
Konstantin Mikheev <mik.kon6610@gmail.com>
2025-01-23 14:02:55 +0100

    fix

--- generate-repos.ts
+++ generate-repos.ts
@@ -19,12 +19,12 @@ const octokit = new Octokit({
 });
 
 const config = {
-  nameStarts: 'ghk_4182_',
+  nameStarts: 'ghk_4167_',
   min: 2,
   max: 2,
-  copyFrom: '../ghk_4182_1',
+  copyFrom: '../ghk_4167_1',
   analyticsVariant: 'var',
-  replaceInFile: 'ghk_4182_1',
+  replaceInFile: 'ghk_4167_1',
 };
 
 const createRepo = async (repoName: string) => {
