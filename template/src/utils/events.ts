2adc615e345b476db2084c5f69722113ced7ca9f
Konstantin Mikheev <mik.kon6610@gmail.com>
2025-03-24 13:59:23 +0100

    fix

--- template/src/utils/events.ts
+++ template/src/utils/events.ts
@@ -1,3 +1,5 @@
+import { LS, LSKeys } from '../ls';
+
 declare global {
   interface Window {
     dataLayer: unknown[];
@@ -25,7 +27,7 @@ export const sendDataToGA = async (payload: Payload) => {
       {
         redirect: 'follow',
         method: 'POST',
-        body: JSON.stringify({ date, ...payload, variant: 'variant1' }),
+        body: JSON.stringify({ date, ...payload, variant: 'variant1', id: LS.getItem(LSKeys.UserId, 0) }),
         headers: {
           'Content-Type': 'text/plain;charset=utf-8',
         },
