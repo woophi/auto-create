fe004eb8a90c4cbaed5ca0bef6c0c552bc026541
Konstantin Mikheev <mik.kon6610@gmail.com>
2025-11-25 07:34:36 +0100

    fix

--- generate-repos.ts
+++ generate-repos.ts
@@ -111,7 +111,7 @@ const runConfig = async (cfg: RepoConfig) => {
       replaceInFile(`../${repoName}/src/ls/index.ts`, cfg.replaceInFile, repoName);
       replaceInFile(`../${repoName}/src/utils/events.ts`, 'variant1', `variant${index}`);
       replaceInFile(`../${repoName}/src/App.tsx`, '_var1', `_var${index}`);
-      replaceInFile(`../${repoName}/src/App.tsx`, 'and_', `ios_`);
+      replaceInFile(`../${repoName}/src/App.tsx`, '6832', `6833`);
 
       await commitChanges(repoName);
     }
